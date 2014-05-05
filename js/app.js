da3d9458e6c45a9272757f8d51668d7d690b9a15
seadrone <gsdrone@gmail.com>
2014-05-05 07:47:16 -0700

    seven integrate filters, addl content, donut chart

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+var expenses = [
+  {
+    value: 7,
+    color:"#e0e0e0"
+  },
+  {
+    value : 93,
+    color : "#48b747"
+  }
+];
+
+var options = {
+  percentageInnerCutout : 74
+};
+
+// var exp = $("#expenses").get(0).getContext("2d");
+// var orgExp = new Chart(exp).Doughnut(expenses, options);
+
+var exp = $("#expenses").get(0).getContext("2d");
+new Chart(exp).Doughnut(expenses, options);
---
+++ js/min/app-ck.js
@@ -0,0 +1,1 @@
+$(document).foundation();var expenses=[{value:7,color:"#e0e0e0"},{value:93,color:"#48b747"}],options={percentageInnerCutout:74},exp=$("#expenses").get(0).getContext("2d");new Chart(exp).Doughnut(expenses,options);
